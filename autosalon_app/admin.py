e14c7dcf3b6b71581c5543ef5d050eae0ad4fca8
Dark <izzxolmatov14@gmail.com>
2025-08-29 11:09:15 +0500

    Добавил фильтры в admin.py

--- autosalon_app/admin.py
+++ autosalon_app/admin.py
@@ -4,3 +4,15 @@ from .models import Brands, Cars
 
 admin.site.register(Brands)
 admin.site.register(Cars)
+
+
+class BrandsAdmin(admin.ModelAdmin):
+    list_display = ("name", "country_of_origin", "year_founded", "founder")
+    list_filter = ("country_of_origin", "year_founded")   # фильтры справа
+    search_fields = ("name", "country_of_origin")        # поиск сверху
+
+
+class CarsAdmin(admin.ModelAdmin):
+    list_display = ("model_name", "car_brand", "year_of_issue", "engine", "body_type", "price")
+    list_filter = ("car_brand", "engine", "body_type", "drive", "price_negotiable")
+    search_fields = ("model_name", "car_brand__name")
